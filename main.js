9aadfc83dba667597ad40b815dd88d363f4b34f5
geroge31415 <kyohei.k31415@icloud.com>
2023-11-18 18:01:24 +0900

    Add files via upload
    test
    値変更

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
-window.addEventListener('DOMContentLoaded', function() {
- 
-    // 実行したい処理を書く
-    console.log("wowowow")
+var total_gas=0;
+var total_elec=0;
 
- 
+var elec_list=[];
+
+window.addEventListener('DOMContentLoaded', function() {
+    console.log("laoded");
+
+    
+    
+
+    
+   
+
+
+
+    
 })
+
+function change(){
+    document.getElementById("total_gas").innerHTML=`ガス総使用量：${total_gas}`;
+    document.getElementById("total_elec").innerHTML=`電力総使用量：${total_elec}`;
+    
+    for(let i=0;i<5;i++){
+        console.log(`elec${i}`);
+        document.getElementById(`elec${i+1}`).innerHTML=elec_list[i];
+    }
+}
+function test(){
+    total_gas=300
+    total_elec=500
+    elec_list=["冷蔵庫：使用料：50%：円","エアコン：使用料：25%：使用円","電子レンジ：使用料：15%：使用円","ドライヤー：使用料：6%：使用円","掃除機：使用料：4%：使用円"];
+    change();
+}
