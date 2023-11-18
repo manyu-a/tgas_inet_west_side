4f7a5227e954b2306ceb61961a75fb88cb2ea6a5
manyu-a <manyumanyu110@gmail.com>
2023-11-18 17:17:50 +0900

    動作できるように変更

--- DummyWatChecker.py
+++ DummyWatChecker.py
@@ -2,25 +2,32 @@ import time
 import random
 import threading
 
+
+
 class DummyWatChecker:
     id: int
     amount: int
     def __init__(self, id: int, amount: int):
         self.id = id
         self.amount = amount
-        pass
+
     def random_change(self):
         while True:
             self.amount = random.randint(50, 100)
-            time.sleep(30)
+            print(self.amount)
+            time.sleep(3)
+    
     def inc_change(self):
         while True:
             self.amount += 1
-            time.sleep(30)
-    thread = threading.Timer(1, random_change)
-    thread.start()
+            time.sleep(3)
+
+    def timerstart(self):
+        thread = threading.Timer(1, self.random_change)
+        thread.start()
 
 wat_checker = DummyWatChecker(0, 0)
+wat_checker.timerstart()
 op = input()
 print(wat_checker.id, wat_checker.amount)
     
