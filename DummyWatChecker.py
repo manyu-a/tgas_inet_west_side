eb9b9d6413f0c9aadd33259b5caf3f6997465dbe
09B21059 <09B21059@localhost>
2023-11-18 17:59:41 +0900

    仮変更

--- DummyWatChecker.py
+++ DummyWatChecker.py
@@ -5,31 +5,44 @@ import threading
 
 
 class DummyWatChecker:
-    id: int
-    amount: int
-    def __init__(self, id: int, amount: int):
+    def __init__(self, id: int, amount: int, randLeft: int, randRight: int, incValue: int):
         self.id = id
         self.amount = amount
+        self.randLeft = randLeft
+        self.randRight = randRight
+        self.incValue = incValue
+        self.timerstart()
 
     def random_change(self):
         while True:
-            self.amount = random.randint(50, 100)
+            self.amount = random.randint(self.randLeft, self.randRight)
             print(self.amount)
-            time.sleep(3)
+            time.sleep(10)
     
     def inc_change(self):
         while True:
-            self.amount += 1
-            time.sleep(3)
+            self.amount += self.incValue
+            time.sleep(10)
 
     def timerstart(self):
         thread = threading.Timer(1, self.random_change)
         thread.start()
 
-wat_checker = DummyWatChecker(0, 0)
-wat_checker.timerstart()
-op = input()
-print(wat_checker.id, wat_checker.amount)
+whole_wat_checker = DummyWatChecker(0, 1, 400, 600, 4)
+tv_wat_checker = DummyWatChecker(1, 0, 50, 100, 1)
+airCon_wat_checker = DummyWatChecker(2, 0, 50, 100, 1)
+oven_wat_checker = DummyWatChecker(3, 0, 50, 100, 1)
+stove_wat_checker = DummyWatChecker(4, 0, 50, 100, 1)
+wat_checker_list = [tv_wat_checker, airCon_wat_checker, oven_wat_checker, stove_wat_checker]
+
+gas_checker = DummyWatChecker(-1, 0, 100, 500, 1)
+
     
-        
+while True:
+    input()
+    print("id: " + whole_wat_checker.id + ", value: " + whole_wat_checker.amount)
+    for wat_checker in wat_checker_list:
+        ratio = wat_checker.amount / whole_wat_checker.amount
+        print("id: " + wat_checker.id + ", value: " + wat_checker.amount + ", " + ratio + "%")
+    print("id: " + gas_checker.id + ", value: " + gas_checker.amount)
     
