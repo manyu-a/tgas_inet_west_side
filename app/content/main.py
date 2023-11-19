5e031ab7ae2bf207c0f1567ac7483bcbc7bb8696
manyu-a <manyumanyu110@gmail.com>
2023-11-19 11:32:42 +0900

    朝昼晩用のインスタンス追加

--- app/content/main.py
+++ app/content/main.py
@@ -16,6 +16,20 @@ name = {1: "TV", 2: "エアコン", 3: "オーブン", 4: "ストーブ", 5: "�
 yen_per_wat = 20
 yen_per_gas = 215
 
+# 朝昼晩用(夜はダミー)
+whole_wat_hour = DummyWatChecker(0, 1, 2000, 2700, 4)
+whole_wat_morning = DummyWatChecker(0, 1, 500, 700, 4)
+whole_wat_daytime = DummyWatChecker(0, 1, 200, 400, 4)
+whole_wat_evening = DummyWatChecker(0, 1, 2, 3, 4)
+
+tv_wat_hour = DummyWatChecker(1, 0, 150, 300, 1)
+airCon_wat_hour = DummyWatChecker(2, 0, 650, 800, 1)
+oven_wat_hour = DummyWatChecker(3, 0, 50, 100, 1)
+stove_wat_hour = DummyWatChecker(4, 0, 50, 100, 1)
+refrigerator_wat_hour = DummyWatChecker(4, 0, 50, 100, 1)
+hour_id =
+hour_checker = {0: whole_wat_hour, 1: tv_wat_hour, 2: airCon_wat_hour,
+           3: oven_wat_hour, 4: stove_wat_hour, 5: refrigerator_wat_hour, -1: gas_checker}
 class User(BaseModel):
     name: str
 
@@ -40,3 +54,7 @@ def wat_all(id : int):
     return { "id": checker[id].id, "full": checker[id].amount, "per": checker[id].amount / checker[0].amount,
             "yen": checker[id].amount * yen_per_wat, "name": name[id]}
 
+@app.post("/api/elec_hour{hour}")
+def wat_hour(hour : int):
+    return { "id": checker[0].id, "full": checker[0].amount * hour, "per": checker[0].amount,
+            "yen": checker[0].amount * yen_per_wat * hour, "name": "電気"}
