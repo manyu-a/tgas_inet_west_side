84ef8feb796f09f09088e414812db86514adfc7c
geroge31415 <kyohei.k31415@icloud.com>
2023-11-19 11:14:22 +0900

    Add files via upload
    totalgas
    totalelec
    追加しました

--- web/content/main.js
+++ web/content/main.js
@@ -1,6 +1,7 @@
 var total_gas=0;
 var total_elec=0;
 
+
 var elec_list=[];
 
 window.addEventListener('DOMContentLoaded', function() {
@@ -19,12 +20,13 @@ window.addEventListener('DOMContentLoaded', function() {
 
 function change(){
     console.log("change");
-    document.getElementById("total_gas").innerHTML=`ガス総使用量：${total_gas}`;
-    document.getElementById("total_elec").innerHTML=`電力総使用量：${total_elec}`;
+    document.getElementById("total_gas").innerHTML=`total_gas`;
+    document.getElementById("total_elec").innerHTML=`total_elec`;
     
-    for(let i=0;i<5;i++){
+    
+    for(let i=1;i<=5;i++){
         console.log(`elec${i}`);
-        document.getElementById(`elec${i+1}`).innerHTML=elec_list[i];
+        document.getElementById(`elec${i}`).innerHTML=elec_list[i];
     }
 }
 function test(){
@@ -33,14 +35,36 @@ function test(){
     // elec_list=["冷蔵庫：50%：4000円","エアコン：25%：3000円","電子レンジ：15%：使用円","ドライヤー：使用量：6%：使用円","掃除機：使用量：4%：使用円"];
     console.log("callApi");
     callApi();
-    helloApi();
 }
 
 //restAPI
 async function callApi() {
     console.log("callApi");
-    for(let i=0;i<5;i++){
-        let res = await fetch(`http://localhost:8000/api/wat_${i}`,{
+    let res_gas_full = await fetch(`http://localhost:8080/api/gas_full`,{
+            method: "POST"
+        })
+            .then(result => result.json())
+            .then((output) => {
+                console.log('Output: ', output);
+                let gas_full = output["full"];
+                let gas_yen  = output["yen"];
+                total_gas="ガス総使用量："+gas_full+"m³："+gas_yen+"円";
+                console.log(total_gas);
+            }).catch(err => console.error(err));
+    let res_elec_full = await fetch(`http://localhost:8080/api/wat_full`,{
+            method: "POST"
+        })
+            .then(result => result.json())
+            .then((output) => {
+                console.log('Output: ', output);
+                let wat_full = output["full"];
+                let wat_yen = output["yen"];
+                total_elec="電力総使用量："+wat_full+"kWh："+wat_yen+"円";
+                console.log(total_elec);
+            }).catch(err => console.error(err));
+
+    for(let i=1;i<=5;i++){
+        let res_elec_item = await fetch(`http://localhost:8080/api/wat_${i}`,{
             method: "POST"
         })
             .then(result => result.json())
@@ -51,7 +75,7 @@ async function callApi() {
                 let wat_yen = output["yen"];
                 let wat_name= output["name"];
                 elec_list[i]=wat_name+"："+wat_full+"kwh："+wat_per+"%："+wat_yen+"円";
-                console.log( String(tmpVal) );
+                console.log( elec_list[i] );
             }).catch(err => console.error(err));
     }
     change();
